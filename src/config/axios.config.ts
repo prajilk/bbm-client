0316ab4b9fdae9a8170c3aff4a73a33176fe9a0e
Prajil K <prajilk20017@gmail.com>
2023-11-04 18:58:42 +0530

    api url changed to onrender

--- src/config/axios.config.ts
+++ src/config/axios.config.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 // https://api-bbm.onrender.com
 // http://localhost:5000
 export default axios.create({
-    baseURL: "https://api-bbm.vercel.app",
+    baseURL: "https://api-bbm.onrender.com",
     withCredentials: true,
 });
