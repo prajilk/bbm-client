091636ad97961fae441a3516088ee8b9805eb0b1
Prajil K <prajilk20017@gmail.com>
2023-11-15 23:25:45 +0530

    updated deletedCount client 3

--- src/lib/api/admin/delete-count.ts
+++ src/lib/api/admin/delete-count.ts
@@ -5,6 +5,7 @@ export async function deleteCount(id: string, user: string) {
     const adminCookie = getCookie("admin");
 
     try {
+        await axios.get("/test");
         const { data } = await axios.delete(`/api/admin/counts/${id}/${user}`, {
             headers: {
                 Cookie: `admin=${adminCookie}`,
