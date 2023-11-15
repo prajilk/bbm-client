777be491ea5d73ab74cc6b7e145d5a7e1ba6ca88
Prajil K <prajilk20017@gmail.com>
2023-11-15 23:36:41 +0530

    remove middle from /delete

--- src/lib/api/admin/delete-count.ts
+++ src/lib/api/admin/delete-count.ts
@@ -1,21 +1,8 @@
 import axios from "@/config/axios.config";
-import { getCookie } from "cookies-next";
 
 export async function deleteCount(id: string, user: string) {
-    const adminCookie = getCookie("admin");
-
     try {
-        await axios.get("/test", {
-            headers: {
-                Cookie: `admin=${adminCookie}`,
-            },
-            withCredentials: true,
-        });
-        const { data } = await axios.delete(`/api/admin/counts/${id}/${user}`, {
-            headers: {
-                Cookie: `admin=${adminCookie}`,
-            },
-        });
+        const { data } = await axios.delete(`/api/admin/counts/${id}/${user}`);
         return data;
     } catch (error) {
         return { deleted: false };
